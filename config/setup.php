9bd0f4ccff7370c609714918e28746f9fcaa392d
JosephKot <joseph@codemode.ca>
2016-06-10 19:57:09 -0700

    Change password back to correct password
    I forgot to reset the password in $dbc before pushing the changes...

--- config/setup.php
+++ config/setup.php
@@ -3,7 +3,7 @@
 
 # Database connection...
 
-$dbc = mysqli_connect('localhost', 'AdminTemp', 'password2', 'codemodecms') OR die('Could not connect because: '.mysqli_connect_error()); // adding the die function kills the page and no other content loads
+$dbc = mysqli_connect('localhost', 'AdminTemp', 'password1', 'codemodecms') OR die('Could not connect because: '.mysqli_connect_error()); // adding the die function kills the page and no other content loads
 
 $site_title = 'CODEMODE.CA';
 $page_title = 'Home';
